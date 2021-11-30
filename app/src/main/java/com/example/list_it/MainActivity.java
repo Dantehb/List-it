3b409cf65bd49e8f5623d23aa30e01c1f0d16011
Dantehb <8nexus68@gmail.com>
2021-11-29 18:20:32 -0600

    Initial commit

---
+++ app/src/main/java/com/example/list_it/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.list_it;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView=findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.contenedor, new HomeFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod=new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment fragment=null;
+
+                    switch (item.getItemId()){
+                        case R.id.home:
+                            fragment=new HomeFragment();
+                            break;
+                        case R.id.notifications:
+                            fragment=new NotificationsFragment();
+                            break;
+                        case R.id.members:
+                            fragment=new MembersFragment();
+                            break;
+                        case R.id.faq:
+                            fragment=new FaqFragment();
+                            break;
+                        case R.id.settings:
+                            fragment=new SettingsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.contenedor, fragment).commit();
+
+                    return true;
+                }
+            };
+}
